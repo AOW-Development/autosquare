4749cecbb2a1304cc4f1681640784206ca6f5610
Shiva-aow <shivashankar@aow.co.in>
2025-09-12 14:27:17 +0530

    16 new seo pages

--- src/app/[slag]/page.tsx
+++ src/app/[slag]/page.tsx
@@ -33,51 +33,66 @@ export default async function SlagPage({ params }: SlagPageProps) {
   const data = DATA.find((item) => item.url === slag);
 
   return (
-    <div className="mt-0 pt-0">
-      <div className="w-full h-[160px] sm:h-[240px] md:h-[320px] relative ">
-        <Image
-          src="/autoparts/banner.png"
-          alt="Account Banner"
-          fill
-          style={{ objectFit: "cover" }}
-          priority
-          quality={100} // 1-100, default is 75
-        />
-      </div>
+    <>
+      {data ? (
+        <div className="mt-0 pt-0">
+          <div className="w-full h-[160px] sm:h-[240px] md:h-[320px] relative">
+            <Image
+              src="/seoBanner.png"
+              alt="Account Banner"
+              fill
+              style={{ objectFit: "cover" }}
+              priority
+              quality={100}
+            />
+            <div className="absolute inset-0 flex items-center justify-center">
+              <h1
+                className="text-white text-2xl sm:text-3xl md:text-4xl font-bold  px-4 py-2"
+                style={{
+                  textTransform: "capitalize",
+                  letterSpacing: "0.03em",
+                }}
+              >
+                {data?.url.replace(/-/g, " ") || "Page not found"}
+              </h1>
+            </div>
+          </div>
 
-      <div className="min-h-screen bg-[#091B33] text-white flex flex-col md:flex-row gap-0 md:gap-8 p-4 lg:p-8">
-        {/* <div className="md:w-1/4">
+          <div className="min-h-screen bg-[#091B33] text-white flex flex-col md:flex-row gap-0 md:gap-8 p-4 lg:p-8">
+            {/* <div className="md:w-1/4">
         <Sidebar activeKey="" />
       </div> */}
-        <div className="flex-1 flex flex-col gap-6">
-          <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 md:px-8 lg:px-12">
-            {/* Breadcrumb */}
-            <div className="flex items-center space-x-2 text-base text-white mb-6 px-2 sm:px-4 md:px-0">
-              <Link
-                href="/"
-                className="flex items-center gap-2 hover:text-white transition-colors"
-              >
-                <Image
-                  src="/account/HouseLine.png"
-                  alt="Home"
-                  width={24}
-                  height={24}
-                />
-                <Image
-                  src="/autoparts/arrows (1).png"
-                  alt="Arrow"
-                  width={16}
-                  height={16}
-                />
-              </Link>
-              {/* <span>&gt;</span> */}
-              <h1>{data?.url.replace(/-/g, " ")}</h1>
-            </div>
+            <div className="flex-1 flex flex-col gap-6">
+              <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 md:px-8 lg:px-12">
+                {/* Breadcrumb */}
+                <div className="flex items-center space-x-2 text-base text-white mb-6 px-2 sm:px-4 md:px-0">
+                  <Link
+                    href="/"
+                    className="flex items-center gap-2 hover:text-white transition-colors"
+                  >
+                    <Image
+                      src="/account/HouseLine.png"
+                      alt="Home"
+                      width={24}
+                      height={24}
+                    />
+                    <Image
+                      src="/autoparts/arrows (1).png"
+                      alt="Arrow"
+                      width={16}
+                      height={16}
+                    />
+                  </Link>
+                  {/* <span>&gt;</span> */}
+                  <span>
+                    {data?.url.replace(/-/g, " ") || "Page not found"}
+                  </span>
+                </div>
 
-            <main className="flex flex-col gap-2">
-              {/* WHO ARE WE? Section */}
-              <section>
-                {/* <h2
+                <main className="flex flex-col gap-2">
+                  {/* WHO ARE WE? Section */}
+                  <section>
+                    {/* <h2
                   className="text-xl md:text-[32px] font-semibold uppercase"
                   style={{
                     fontFamily: "Audiowide, sans-serif",
@@ -86,13 +101,13 @@ export default async function SlagPage({ params }: SlagPageProps) {
                 >
                   WHO ARE WE?
                 </h2> */}
-                <div className="flex flex-col md:flex-row items-start">
-                  <div className="flex-1">
-                    <p className="text-base text-gray-200 leading-relaxed mt-4 text-[16px] justify-left">
-                      {data?.content}
-                    </p>
-                  </div>
-                  {/*
+                    <div className="flex flex-col md:flex-row items-start">
+                      <div className="flex-1">
+                        <div className="text-base text-gray-200 leading-relaxed mt-4 text-[16px] justify-left whitespace-pre-line">
+                          {data?.content || "Page not found"}
+                        </div>
+                      </div>
+                      {/*
                   Key changes for mobile responsiveness:
                   - Removed the global -mt-48.
                   - Added mt-4 for mobile to provide some space below the text.
@@ -100,125 +115,151 @@ export default async function SlagPage({ params }: SlagPageProps) {
                   - Added md:relative and md:z-10 to ensure desktop positioning.
                   - Removed z-10 and relative from the mobile view, as they are not needed and can sometimes cause issues when not carefully managed.
                 */}
-                  <div className="flex-1 flex justify-center md:justify-center mt-4 md:-mt-48 md:z-10 md:relative">
-                    <Image
-                      src="/Images/car.png"
-                      alt="Car"
-                      width={490}
-                      height={410}
-                      className="rounded-2xl w-full h-auto max-w-[400px] md:max-w-none" /* Added w-full h-auto for mobile scaling, and max-w- to control size on mobile */
-                    />
-                  </div>
-                </div>
-              </section>
+                      <div className="flex-1 flex justify-center md:justify-center mt-4 md:-mt-48 md:z-10 md:relative">
+                        <Image
+                          src="/Images/car.png"
+                          alt="Car"
+                          width={490}
+                          height={410}
+                          className="rounded-2xl w-full h-auto max-w-[400px] md:max-w-none" /* Added w-full h-auto for mobile scaling, and max-w- to control size on mobile */
+                        />
+                      </div>
+                    </div>
+                  </section>
 
-              {/* OUR GOAL Section */}
-              <section className=" mt-10">
-                <h2
-                  className="text-xl md:text-[32px] font-semibold uppercase mb-4 md:mb-6"
-                  style={{
-                    fontFamily: "Audiowide, sans-serif",
-                    letterSpacing: "0.1em",
-                  }}
-                >
-                  OUR GOAL
-                </h2>
-                <p className="text-base text-gray-200 leading-relaxed mb-6 text-[16px]">
-                  To make the buying process as seamless and worry-free as
-                  possible for our customers and to provide after-sales service
-                  in a professional manner. We want to treat you like we would
-                  want to be treated.
-                </p>
-              </section>
+                  {/* OUR GOAL Section */}
+                  <section className=" mt-10">
+                    <h2
+                      className="text-xl md:text-[32px] font-semibold uppercase mb-4 md:mb-6"
+                      style={{
+                        fontFamily: "Audiowide, sans-serif",
+                        letterSpacing: "0.1em",
+                      }}
+                    >
+                      OUR GOAL
+                    </h2>
+                    <p className="text-base text-gray-200 leading-relaxed mb-6 text-[16px]">
+                      To make the buying process as seamless and worry-free as
+                      possible for our customers and to provide after-sales
+                      service in a professional manner. We want to treat you
+                      like we would want to be treated.
+                    </p>
+                  </section>
 
-              {/* Feature Grid */}
-              <section>
-                <div className="grid grid-cols-1 md:grid-cols-2 mb-10">
-                  <div className="bg-[#091627] rounded-l shadow-sm p-6 mb-4">
-                    <h3 className="font-semibold text-lg mb-2">
-                      Shipping's on Us
-                    </h3>
-                    <p className="text-gray-200">
-                      Get your ordered parts delivered expeditiously. Initial
-                      shipping on ALL Orders is FREE! (*T&C Apply*)
-                    </p>
-                  </div>
-                  <div className="relative bg-[#091627] rounded-r shadow-sm p-4 mb-4 overflow-hidden">
-                    {/* add bg img here : about/fst-card.png */}
-                    <Image
-                      src="/about/Mask group.png"
-                      alt=""
-                      fill
-                      className="object-cover z-0 pointer-events-none"
-                      quality={70}
-                    />
-                    <div className="relative z-10">
-                      <h3 className="font-semibold text-lg mb-2">
-                        Easy Replacements
-                      </h3>
-                      <p className="text-gray-200">
-                        Enjoy peace of mind with easy returns and replacements.
-                      </p>
+                  {/* Feature Grid */}
+                  <section>
+                    <div className="grid grid-cols-1 md:grid-cols-2 mb-10">
+                      <div className="bg-[#091627] rounded-l shadow-sm p-6 mb-4">
+                        <h3 className="font-semibold text-lg mb-2">
+                          Shipping's on Us
+                        </h3>
+                        <p className="text-gray-200">
+                          Get your ordered parts delivered expeditiously.
+                          Initial shipping on ALL Orders is FREE! (*T&C Apply*)
+                        </p>
+                      </div>
+                      <div className="relative bg-[#091627] rounded-r shadow-sm p-4 mb-4 overflow-hidden">
+                        {/* add bg img here : about/fst-card.png */}
+                        <Image
+                          src="/about/Mask group.png"
+                          alt=""
+                          fill
+                          className="object-cover z-0 pointer-events-none"
+                          quality={70}
+                        />
+                        <div className="relative z-10">
+                          <h3 className="font-semibold text-lg mb-2">
+                            Easy Replacements
+                          </h3>
+                          <p className="text-gray-200">
+                            Enjoy peace of mind with easy returns and
+                            replacements.
+                          </p>
+                        </div>
+                      </div>
+                      <div className="bg-[#091627] rounded-l shadow-sm p-8 mb-4">
+                        <h3 className="font-semibold text-lg mb-2">
+                          Timely Delivery
+                        </h3>
+                        <p className="text-gray-200">
+                          We strive and fulfill all our customer orders in a
+                          timely manner.
+                        </p>
+                      </div>
+                      <div className="relative bg-[#091627] rounded-r shadow-sm p-4 mb-4 overflow-hidden">
+                        {/* add bg img here : about/card-btn.png */}
+                        <Image
+                          src="/about/card-betn.png"
+                          alt=""
+                          fill
+                          className="object-cover z-0 pointer-events-none"
+                          quality={70}
+                        />
+                        <div className="relative z-10">
+                          <h3 className="font-semibold text-lg mb-2">
+                            Warranty
+                          </h3>
+                          <p className="text-gray-200">
+                            We provide a standard warranty on all Engines &
+                            Transmissions sold by us.
+                          </p>
+                        </div>
+                      </div>
+                      <div className="bg-[#091627] rounded-l shadow-sm p-8 mb-4">
+                        <h3 className="font-semibold text-lg mb-2">
+                          Reliability
+                        </h3>
+                        <p className="text-gray-200">
+                          We offer reliable and quality used OEM Auto Parts,
+                          Engines, and Transmissions.
+                        </p>
+                      </div>
+                      <div className="relative bg-[#091627] rounded-r shadow-sm p-4 mb-4 overflow-hidden">
+                        {/* add bg img here : about/Mask group.png */}
+                        <Image
+                          src="/about/fst-card.png"
+                          alt=""
+                          fill
+                          className="object-cover z-0 pointer-events-none"
+                          quality={70}
+                        />
+                        <div className="relative z-10">
+                          <h3 className="font-semibold text-lg mb-2">
+                            Competitive Pricing
+                          </h3>
+                          <p className="text-gray-200">
+                            We provide competitive pricing on all our products
+                            with multiple options to meet your budget.
+                          </p>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div className="bg-[#091627] rounded-l shadow-sm p-8 mb-4">
-                    <h3 className="font-semibold text-lg mb-2">
-                      Timely Delivery
-                    </h3>
-                    <p className="text-gray-200">
-                      We strive and fulfill all our customer orders in a timely
-                      manner.
-                    </p>
-                  </div>
-                  <div className="relative bg-[#091627] rounded-r shadow-sm p-4 mb-4 overflow-hidden">
-                    {/* add bg img here : about/card-btn.png */}
-                    <Image
-                      src="/about/card-betn.png"
-                      alt=""
-                      fill
-                      className="object-cover z-0 pointer-events-none"
-                      quality={70}
-                    />
-                    <div className="relative z-10">
-                      <h3 className="font-semibold text-lg mb-2">Warranty</h3>
-                      <p className="text-gray-200">
-                        We provide a standard warranty on all Engines &
-                        Transmissions sold by us.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="bg-[#091627] rounded-l shadow-sm p-8 mb-4">
-                    <h3 className="font-semibold text-lg mb-2">Reliability</h3>
-                    <p className="text-gray-200">
-                      We offer reliable and quality used OEM Auto Parts,
-                      Engines, and Transmissions.
-                    </p>
-                  </div>
-                  <div className="relative bg-[#091627] rounded-r shadow-sm p-4 mb-4 overflow-hidden">
-                    {/* add bg img here : about/Mask group.png */}
-                    <Image
-                      src="/about/fst-card.png"
-                      alt=""
-                      fill
-                      className="object-cover z-0 pointer-events-none"
-                      quality={70}
-                    />
-                    <div className="relative z-10">
-                      <h3 className="font-semibold text-lg mb-2">
-                        Competitive Pricing
-                      </h3>
-                      <p className="text-gray-200">
-                        We provide competitive pricing on all our products with
-                        multiple options to meet your budget.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </section>
-            </main>
+                  </section>
+                </main>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-[#0A1A2F] text-white">
+          <Image
+            src="/404.png"
+            alt="404 Not Found"
+            width={996}
+            height={456}
+            style={{ maxWidth: "90vw", height: "auto" }}
+            priority
+          />
+          <div className="mt-8 text-center">
+            <h1 className="text-2xl font-bold mb-2 uppercase">
+              Page not found
+            </h1>
+            <Link href="/" className="text-sky-400 hover:underline text-lg">
+              Go Home
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -43,6 +43,109 @@ export const metadataMap: Record<string, Metadata> = {
     keywords:
       "car engines for sale near me in United States, buy car engines near me, used car engines USA, rebuilt car engines for sale, affordable car engines near me, replacement car engines USA, quality car engines for sale, cheap car engines near me, new and used car engines USA, car engine suppliers near me",
   },
+  "/cheap-engines-for-sale-in-united-states": {
+    title: "Cheap Engines for Sale in United States – Budget Friendly Price",
+    description:
+      "Explore reliable and cheap engines for sale in United States by Parts Central. Get quality, affordable replacement engines with trusted performance and quick delivery.",
+    keywords:
+      "Explore reliable and cheap engines for sale in United States by Parts Central. Get quality, affordable replacement engines with trusted performance and quick delivery.",
+  },
+  "/cheap-transmissions-in-united-states": {
+    title: "Cheap Transmissions in United States | Buy Used Transmissions",
+    description:
+      "Get Cheap Transmissions in United States, Find our best fitting automatic transmissions for your vehicle and enjoy free next day delivery.",
+    keywords:
+      "cheap transmissions in United States, used transmissions, wholesale transmissions,Parts Central transmissions, buy transmissions near me, used transmissions USA, rebuilt transmissions for sale, affordable car transmissions, replacement transmissions near me, new and used transmissions USA, car transmission suppliers United States, cheap automatic transmissions USA",
+  },
+  "/rebuilt-transmissions-for-sale-in-united-states": {
+    title: "Rebuilt Transmissions for Sale in US | Assured Warranty",
+    description:
+      "Shop rebuilt transmissions for sale in United States at Parts Central. Affordable, reliable, and certified quality auto parts to keep your vehicle running strong.",
+    keywords:
+      "rebuilt transmissions for sale in United States, rebuilt transmissions near me, buy rebuilt transmissions USA, affordable rebuilt transmissions, quality rebuilt transmissions, certified rebuilt transmissions USA, rebuilt auto transmissions for sale, Parts Central transmissions, used and rebuilt transmissions USA, best rebuilt transmissions deals",
+  },
+  "/quality-used-transmissions-in-united-states": {
+    title: "Quality Used Transmissions in US | Affordable Prices and Tested",
+    description:
+      "We offer high-quality used Quality Used Transmissions in United States that are both affordable and reliable. Our team carefully inspects each transmission.",
+    keywords:
+      "quality used transmissions in United States, Parts Central transmissions, affordable used transmissions USA, used transmissions near me, buy car transmissions USA, rebuilt transmissions for sale, replacement transmissions United States, cheap used transmissions near me, tested used transmissions USA, best used transmissions suppliers",
+  },
+  "/used-car-engines-for-sale-in-united-states": {
+    title: "Used Car Engines for Sale in US | Quality Tested with Warranty",
+    description:
+      "Get reliable quality used transmissions in United States from Parts Central. Affordable, durable, and trusted options to keep your vehicle running smoothly.",
+    keywords:
+      "quality used transmissions in United States, Parts Central transmissions, affordable used transmissions USA, used transmissions near me, buy car transmissions USA, rebuilt transmissions for sale, replacement transmissions United States, cheap used transmissions near me, tested used transmissions USA, best used transmissions suppliers",
+  },
+  "/used-motors-for-sale-near-me-in-united-states": {
+    title: "Find Affordable Used Motors for Sale Near Me in United States",
+    description:
+      "Shop our Used Motors for Sale Near Me in United States. We have a variety of car engines for Fords, GMC, Cadillac, Hyundai, GMC, Audi, and more.",
+    keywords:
+      "used motors for sale near me in United States, buy used motors USA, Parts Central used motors, affordable used motors near me, quality used motors United States, certified used motors for sale, used auto motors near me, reliable used motors USA, replacement used motors United States",
+  },
+  "/used-350-chevy-engine-for-sale-in-united-states": {
+    title: "Used 350 Chevy Engine and Motor for Sale in US | Car Parts",
+    description:
+      "Find reliable Used 350 Chevy Engine for Sale in United States at Car Parts. Quality tested, affordable engines with nationwide shipping available.",
+    keywords:
+      "Used 350 Chevy Engine for Sale in United States, Chevy 350 engine used, affordable Chevy 350 engine, buy used Chevy engine USA, Car Parts",
+  },
+  "/700-r4-transmission-for-sale-in-united-states": {
+    title: "700 R4 Transmission for Sale in USA | Buy Now",
+    description:
+      "Shop 700 R4 Transmission for Sale in United States at Car Parts. Get high-quality, durable, and affordable transmissions with reliable nationwide shipping.",
+    keywords:
+      "700 R4 Transmission for Sale, 700 R4 Transmission USA, Buy 700 R4 Transmission, Car Parts Transmission, Affordable 700 R4 Transmission, Transmission for Sale Near Me",
+  },
+  "/4l60e-transmission-4x4-in-united-states": {
+    title: "4L60E Transmission 4x4 in United States | New, Used & Rebuilt",
+    description:
+      "4L60E transmission problems? We have replacement new, used, & rebuilt transmissions for sale with fast shipping and assured warranty.",
+    keywords:
+      "4L60E Transmission 4x4, 4L60E Transmission for Sale, 4x4 Transmission USA, Car Parts Transmission, Buy 4L60E Transmission Online",
+  },
+  "/jeep-wrangler-automatic-transmission-for-sale-in-united-states": {
+    title: "Jeep Wrangler Automatic Transmission for Sale in United States",
+    description:
+      "Browse the best deals on Jeep Wrangler Automatic Transmission for Sale in United States Great deals. Massive selection from top brands on Partscentral",
+    keywords:
+      "Jeep Wrangler automatic transmission, Jeep transmission for sale, Jeep Wrangler transmission USA, Jeep parts online, Jeep Wrangler gearbox",
+  },
+  "/honda-civic-engine-for-sale-in-united-states": {
+    title: "Honda Civic Engine for Sale in United States | Prices & Model",
+    description:
+      "Honda Civic Engine for Sale in United States, Explore our collection of top-notch engines. Shop now and find the perfect engine for your vehicle",
+    keywords:
+      "Honda Civic engine for sale, Honda Civic used engine, Honda Civic replacement engine, Honda Civic motor for sale, buy Honda Civic engine USA",
+  },
+  "/toyota-transmission-in-united-states": {
+    title: "Toyota Transmission in United States | Car Parts Price | Buy Now",
+    description:
+      "Find high-quality Toyota Transmission in United States at Car Parts. Reliable, durable, and affordable auto solutions with nationwide delivery.",
+    keywords:
+      "Toyota transmission, Toyota transmission in United States, Toyota auto parts, Toyota gearbox, Toyota transmission for sale, buy Toyota transmission, Toyota replacement parts, Toyota transmission online, Toyota spare parts, Toyota engine and transmission",
+  },
+  "/best-place-to-buy-used-car-engines-in-united-states": {
+    title: "Best Place to Buy Used Car Engines in US | Assured Low Prices",
+    description:
+      "Car Parts offers the best place to buy used car engines in United States with quality, affordable prices, and reliable nationwide delivery.",
+    keywords:
+      "best place to buy used car engines in United States, used car engines for sale, affordable used car engines, car parts USA, buy used engines online",
+  },
+  "/dodge-ram-2500-cummins-turbo-diesel-for-sale-in-united-states": {
+    title: "Dodge Ram 2500 Cummins Turbo Diesel for Sale | USA",
+    description:
+      "Find the best deals on Dodge Ram 2500 Cummins Turbo Diesel for Sale in the United States at Car Parts. Reliable engines, powerful performance, affordable price.",
+    keywords:
+      "Dodge Ram 2500 Cummins Turbo Diesel for Sale, Dodge Ram Cummins USA, Ram 2500 Turbo Diesel for Sale, Dodge Ram 2500 Diesel Truck",
+  },
+  // "/4l60e-transmission-4x4-in-united-states":{
+  //   title:"Quality Used Transmissions in US | Affordable Prices and Tested",
+  //   description:"",
+  //   keywords:"",
+  // },
 };
 
 export const defaultMetadata: Metadata = {

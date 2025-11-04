cf1043f78bccc0c7d4f3cd82a197cb03d9f2f1bc
Kubra709 <dev@aow.co.in>
2025-11-04 16:08:04 +0530

    asdfdf

--- src/app/account/thankYou/page.tsx
+++ src/app/account/thankYou/page.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image"
+
 import Link from "next/link"
 import { useEffect, useRef, useState } from "react"
 import useBillingStore from "@/store/billingStore"
